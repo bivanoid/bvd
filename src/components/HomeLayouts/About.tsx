1fe3aa6babc2828c5038c7efc742460c9659273b
bivanoid <abivandyafirdhan@gmail.com>
2026-08-23 18:42:26 +0700

    uptd

--- src/components/HomeLayouts/About.tsx
+++ src/components/HomeLayouts/About.tsx
@@ -1,20 +1,22 @@
 import s from "./about.module.css";
 
 export default function About() {
-    return (
-        <section className={s.about}>
-                <h2>
-                    Hello! My name is Firdhan Abivandya <span className="red">★</span>. I am from
-                    Surakarta, Central Java Province, Indonesia.
-                    <br />
-                    I like to pour my imagination into code and implement it.
-                </h2>
-                <ul>
-                    <li><span className="red">[</span>University<span className="red">]</span></li>
-                    <li>Pignateli Triputra University [IDN]</li>
-                    <li>Software Engineering</li>
-                    <li>Surakarta City</li>
-                </ul>
-        </section>
-    )
+	return (
+		<section className={s.about}>
+			<h2>
+				Hello! My name is Firdhan Abivandya <span className="red">★</span>.
+				I am from Surakarta, Central Java Province, Indonesia.
+				<br />I like to pour my imagination into code and implement it.
+			</h2>
+			<ul>
+				<li>
+					<span className="red">[</span>University
+					<span className="red">]</span>
+				</li>
+				<li>Pignateli Triputra University [IDN]</li>
+				<li>Software Engineering</li>
+				<li>Surakarta City</li>
+			</ul>
+		</section>
+	);
 }
